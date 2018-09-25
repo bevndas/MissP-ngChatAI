72baddfba095b6e6958bf6d5c852c0a4d37b53b3
Bipin <bipinshrestha0018@gmail.com>
2018-09-25 14:36:40 +0545

    Message UI, connected to  Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+export const DEFAULT_TTS_HOST = 'https://api.api.ai/api/tts';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    dialogflow: {
+    angularBot:  'c33aca402634496fbeb49e87a8dc0754'
+    }
 };
 
 /*
